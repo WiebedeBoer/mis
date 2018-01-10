567ceb108d6d9874f30fefba5d5c37217f60b903
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-01-10 15:12:36 +0100

    install 0.2
    versie 0.2

--- install.php
+++ install.php
@@ -1,40 +1,63 @@
 <?php
 
+include("includes/connect.php");
 
-$uquery = "UPDATE Users SET Cokey ='$makekey' WHERE Username = ?";
-$uid = $conn->prepare($uquery);
-$uid->bind_param('s', $username);
-$uid->execute();
-$uid->close();
+/*SQL CONNECTION*/
+$conn = new mysqli($myserver, $myname, $mypassword, $mydb);
+if ($conn->connect_error)
+  {
+
+echo "
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<title>error</title>
+</HEAD>
+<BODY>
+<P class='error'>Could not select database</P>
+</BODY>
+</HTML>";
+  }
+else {
+
+
+
+if (isset($_POST["user"]) && isset($_POST["pass"])) {
+if (filter_var($_POST["user"], FILTER_SANTITIZE_STRING)){
+if (filter_var($_POST["pass"], FILTER_SANTITIZE_STRING)){
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+echo '<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<title>installer</title>
+</HEAD>
+<BODY>'
+
+
 
 
 $bquery = "CREATE TABLE `Bestanden` (`ID` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY(ID), `URL` varchar(240) NOT NULL)";
 $bid = $conn->prepare($bquery);
 $bid->execute();
 $bid->close();
-
-
 $cquery = "CREATE TABLE `Brute` (`ID` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY(ID), `User` varchar(80) NOT NULL, `Timer` int(11) NOT NULL)";
 $cid = $conn->prepare($cquery);
 $cid->execute();
 $cid->close();
-
-
 $dquery = "CREATE TABLE `Photos` (`ID` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY(ID), `Imgurl` varchar(240) NOT NULL, `Imgname` varchar(240) NOT NULL, `Width` int(11) NOT NULL, `Height` int(11) NOT NULL, `Photosize` int(11) NOT NULL)";
 $did = $conn->prepare($dquery);
 $did->execute();
 $did->close();
-
 $equery = "CREATE TABLE `Seo` (`ID` int(1) NOT NULL AUTO_INCREMENT,  PRIMARY KEY(ID),  `Titel` varchar(80) NOT NULL, `Beschrijving` varchar(200) NOT NULL, `Zoektermen` varchar(240) NOT NULL, `Bannier` varchar(240) NOT NULL)";
 $eid = $conn->prepare($equery);
 $eid->execute();
 $eid->close();
-
 $fquery = "CREATE TABLE `Users` (`ID` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY(ID), `Username` varchar(40) NOT NULL, `Password` varchar(20) NOT NULL, `Category` varchar(20) NOT NULL, `Cokey` varchar(20) NOT NULL DEFAULT '0')";
 $fid = $conn->prepare($fquery);
 $fid->execute();
 $fid->close();
-
 $gquery = "CREATE TABLE `Webcontent` (`ID` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY(ID), `Pagina` varchar(80) NOT NULL, `URL` varchar(80) NOT NULL, `Tekst` text NOT NULL, `Verwijderbaar` int(1) NOT NULL DEFAULT '0',  `Zichtbaar` int(1) NOT NULL DEFAULT '1')";
 $gid = $conn->prepare($gquery);
 $gid->execute();
@@ -59,9 +82,6 @@ ALTER TABLE `Webcontent` ADD UNIQUE KEY `unique` (`ID`);
 
 ALTER TABLE `Bestanden` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `Brute` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `Meldingen` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `Photos` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `Users` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 */
 
 $hquery = "INSERT INTO `Webcontent` (`ID`, `Pagina`, `URL`, `Tekst`, `Verwijderbaar`, `Zichtbaar`) VALUES (1, 'Home', 'index.php', 'home', 0, 1)";
@@ -77,5 +97,55 @@ $kid = $conn->prepare($kquery);
 $kid->execute();
 $kid->close();
 
+$lquery = "INSERT INTO `Seo` (`ID`, `Titel`, `Beschrijving`, `Zoektermen`, `Bannier`) VALUES (0, 'Test', 'leeg', 'leeg', 'leeg')";
+$lid = $conn->prepare($lquery);
+$lid->execute();
+$lid->close();
+
+$mquery = "INSERT INTO `Users` (`ID`, `Username`, `Password`, `Category`, `Cokey`) VALUES
+(1, ?, ?, 'superadmin', '0')";
+$mid = $conn->prepare($mquery);
+$mid->bind_param('ss', $username, $password);
+$mid->execute();
+$mid->close();
+
+}
+else {
+echo '<p class="error">invalid password</p>';
+}
+}
+else {
+echo '<p class="error">invalid username</p>';
+}
+}
+else {
+
+$cquery = "SELECT COUNT(*) AS usercheck FROM Users";
+$cid = $conn->prepare($cquery);
+$cid->execute();
+$cid->bind_result($usercheck);
+$cid->fetch();
+$cid->close();
+
+if ($usercheck <1){
+
+echo '<form method="Post" action="install.php">
+Username: <input type="text" size=20 maxlength=20 name="user">
+Password: <input type="text" size=20 maxlength=20 name="pass">
+<INPUT type="Submit" value="create">
+</form>';
+
+}
+else {
+echo '<p class="error">already installed</p>';
+}
+
+}
+
+
+echo '</BODY>
+</HTML>';
+
+}
 
 ?>
