55996768c55003a29bb2083bebf75adc7e6883ad
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-01-17 13:08:32 +0100

    sitemap maker added
    sitemap maker added

--- admin/sitemap.php
+++ admin/sitemap.php
@@ -20,6 +20,41 @@ if (isset($_POST["url"]) && isset($_POST["changefreq"])){
 $newurl = $_POST["url"];
 $newfre = $_POST["changefreq"];
 
+$cquery = "SELECT Domein FROM SEO WHERE ID ='1'";
+$cid = $conn->prepare($cquery);
+$cid->execute();
+$cid->bind_result($domein);
+$cid->fetch();
+$cid->close();
+
+$lquery = "INSERT INTO `Sitemap` (`URL`, `Freq`) VALUES (?, ?)";
+$lid = $conn->prepare($lquery);
+$lid->bind_param('ss', $newurl, $newfre);
+$lid->execute();
+$lid->close();
+
+$write_txt '&lt;urlset&gt;';
+
+$mwquery = "SELECT * FROM Sitemap ORDER BY ID";
+$result_wpg = $conn->query($mwquery);
+while ($rowwpg = $result_wpg->fetch_assoc())
+  {
+$weburl = $rowwpg['URL'];
+$webfre = $rowwpg['Freq'];
+
+$write_txt= $write_txt.'&lt;url>
+&lt;loc&gt;'.$domein.'/&lt;'.$weburl.'/loc&gt;
+&lt;lastmod>'.date("Y").'-'.date("m").'-'.date("d").'T'.date("H").':'.date("i").':'.date("s").'+00:00&lt;/lastmod&gt;
+&lt;changefreq>'.$webfre.'&lt;/changefreq&gt;
+&lt;/url&gt;';
+   }
+
+$write_txt= $write_txt.'&lt;/urlset&gt;';
+
+$myfile = fopen("../sitemap.xml", "w") or die("Unable to open file!");
+fwrite($myfile, $write_txt);
+fclose($myfile);
+
 
 }
 
