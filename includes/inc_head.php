ece55e9066837c6ef6f66fcb2895d326fe6f8c05
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-01-19 11:54:21 +0100

    fixed styling
    fixed styling

--- includes/inc_head.php
+++ includes/inc_head.php
@@ -11,11 +11,11 @@ $searchterms_text = $row_seo['Zoektermen'];
 $search_id = 1;
 
 //select
-$cquery = "SELECT Titel, Beschrijving, Zoektermen FROM Seo WHERE ID = ?";
+$cquery = "SELECT Titel, Beschrijving, Zoektermen, Style FROM Seo WHERE ID = ?";
 $cid = $conn->prepare($cquery);
 $cid->bind_param('i', $search_id);
 $cid->execute();
-$cid->bind_result($describe_title, $describe_text, $searchterms_text);
+$cid->bind_result($describe_title, $describe_text, $searchterms_text, $stylesheet);
 $cid->fetch();
 $cid->close();
 //close connection
@@ -29,7 +29,7 @@ echo '<title>'.$describe_title.'</title>
 <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
 <meta http-equive="content-type" content="text/html; charset=ISO-8859-1">
 <link rel="shortcut icon" type="image/png" href="favicon.png">
-<link rel="stylesheet" type="text/css" href="styles/web_style20.css">
+<link rel="stylesheet" type="text/css" href="styles/'.$stylesheet.'.css">
 <meta name=viewport content="width=device-width, initial-scale=1">
 <script type="text/javascript" src="scripts/jquery.js"></script>
 <script type="text/javascript" src="scripts/script.js"></script>';
