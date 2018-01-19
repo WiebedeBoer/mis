1adba05728437782346cb4f59fadecb738b86f8c
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-01-19 10:49:47 +0100

    versie 0.4
    fixes van docx extensie en bruteforce

--- admin/bestand_loader.php
+++ admin/bestand_loader.php
@@ -7,20 +7,11 @@ include("head.php");
 </HEAD>
 <BODY>
 <?php
-
 include("connect.php");
 if ($connected ==1){
-
 echo '<h1>Bestanden</h1>';
-
 echo '<a href="beheer.php" title="Beheer" class="bl">Terug naar Beheer</a>';
-
-
-
-
-
-
-if (($_FILES["file"]["type"] == "application/doc") || ($_FILES["file"]["type"] == "application/docx") || ($_FILES["file"]["type"] == "application/pdf") || ($_FILES["file"]["type"] == "application/rtf")){
+if (($_FILES["file"]["type"] == "application/doc") || ($_FILES["file"]["type"] == "application/docx") || ($_FILES["file"]["type"] == "application/pdf") || ($_FILES["file"]["type"] == "application/rtf") || $_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"){
  if ($_FILES["file"]["size"] < 10000000){
    if ($_FILES["file"]["error"] > 0)
      {
@@ -31,12 +22,10 @@ if (($_FILES["file"]["type"] == "application/doc") || ($_FILES["file"]["type"] =
 $rfilename = $_FILES["file"]["name"];
 $qfilename = str_replace("'","_",$rfilename);
 $_FILES["file"]["name"] = $qfilename;
-
      echo "<P>Upload: " . $_FILES["file"]["name"] . "</P>";
      echo "<P>Bestandstype: " . $_FILES["file"]["type"] . "</P>";
      echo "<P>Grootte: " . ($_FILES["file"]["size"] / 1024) . " Kb</P>";
      echo "<P>Temp file: " . $_FILES["file"]["tmp_name"] . "</P>";
-
     if (file_exists("../files/" . $_FILES["file"]["name"]))
        {
        echo "<P class='error'>Het ". $_FILES["file"]["name"] ." bestand bestaat al.<BR>Terug naar <A HREF='beheer.php'>beheer</A></P>";
@@ -56,34 +45,25 @@ $_FILES["file"]["name"] = $qfilename;
         $iid->bind_param('s', $tabfullname);
         $iid->execute();
         $iid->close();
-       
+
        echo "<P>Grootte (in Bytes): ".$imgsize."</P>";
        echo "<P>Opgeslagen in: " . "../files/" . $_FILES["file"]["name"] ."</P>";
-
        echo "<P><b>Vervang het woord link in de onderste regel met woorden die je wilt hebben!</b></P>";
        echo "<P><b>Kopieer de volgende regel en plak deze in de editor waar je het wil hebben!</b></P>";
        echo "<P>[url ='../files/" . $_FILES["file"]["name"] ."']link[/url]</P>";
        }
      }   /*copyright wiebe eling de boer*/
-
 }
 else {
 echo "<P class='error'>Bestand is te groot. Het bestand mag niet groter zijn dan 10 Megabyte.<BR>Terug naar <A HREF='beheer.php'>beheer</A></P>";
 }
-
 }
 else {
 echo "<P class='error'>Ongeldig bestand type. Bestand moet een van de volgende extensies hebben: doc, docx, pdf, rtf<BR>Terug naar <A HREF='beheer.php'>beheer</A></P>";
 }
-
-
-
-
-
 //SQL CONNECTIE SLUITEN
 //mysql_close($con);
 }
-
 ?>
 </BODY>
 </HTML>
--- admin/portal.php
+++ admin/portal.php
@@ -1,31 +1,20 @@
 <?php
-
 if (isset($_POST["username"]) && isset($_POST["password"])){
-
 /*FILTERS*/
-
 //CHECK USERNAME
 if(filter_has_var(INPUT_POST, "username")){
-
 //CHECK PASSWORD
 if(filter_has_var(INPUT_POST, "password")){
-
 if (filter_var($_POST["username"], FILTER_SANITIZE_STRING)){
-
 if (filter_var($_POST["password"], FILTER_SANITIZE_STRING)){
-
 $username = $_POST["username"];
-$password = $_POST["password"];
-
+$password = md5($_POST["password"]);
 //DATABASE CONNECTION VARIABLES
 include("../includes/connect.php");
-
 // Create connection
 $conn = new mysqli($myserver, $myname, $mypassword, $mydb);
-
 // Check connection
 if ($conn->connect_error) {
-
 echo '<!DOCTYPE HTML>
 <HTML>
 <HEAD>';
@@ -38,13 +27,11 @@ die("Connection failed: " . $conn->connect_error);
 echo "<P class='error'>Could not connect</P>";
   }
 else {
-
 /*
 //DATABASE SELECTION
 $selector = mysql_select_db($mydb, $con);
 if (!$selector)
 {
-
 echo '<!DOCTYPE HTML>
 <HTML>
 <HEAD>';
@@ -56,17 +43,24 @@ die('Could not select database: ' . mysql_error());
 echo "<P class='error'>Could not select database</P>";
 echo '</BODY>
 </HTML>';
-
 }
 else {
 */
-
 /*
 $resultco = mysql_query ("SELECT COUNT(*) AS usercount FROM Users WHERE Username ='$username'");
 $rowco = mysql_fetch_assoc($resultco);
 $usercheck = $rowco["usercount"];
 */
-
+//bruteforce protection
+$brutequery = "SELECT tries FROM Brute WHERE User = ? AND DATE_ADD(block_time, INTERVAL 30 MINUTE) >= NOW()";
+$brute = $conn->prepare($brutequery);
+$brute->bind_param('s', $username);
+$brute->execute();
+$brute->bind_result($tries);
+$brute->fetch();
+$brute->close();
+// blockeer inloggen als gebruiker 5x heeft geprobeert in te loggen.
+if ($tries < 5){
 //COUNT USER
 $cquery = "SELECT COUNT(*) AS usercheck, ID, Password, Cokey FROM Users WHERE Username = ?";
 $cid = $conn->prepare($cquery);
@@ -75,21 +69,15 @@ $cid->execute();
 $cid->bind_result($usercheck, $user_id, $gpass, $makekey);
 $cid->fetch();
 $cid->close();
-
 if ($usercheck ==1){
-
 /*
 $result = mysql_query ("SELECT * FROM Users WHERE Username ='$username'");
 $row = mysql_fetch_assoc($result);
 $tabpass = $row["Password"];
-
 if ($password == $tabpass){
 */
-
 if ($password == $gpass){
-
 /*COOKIE RANDOM KEY*/
-
 $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 $ra = (rand(1,62)) - 1;
 $rancha = substr($chars,$ra,1);
@@ -115,64 +103,81 @@ $rk = (rand(1,62)) - 1;
 $ranchk = substr($chars,$rk,1);
 $rl = (rand(1,62)) - 1;
 $ranchl = substr($chars,$rl,1);
-
 $makekey = $rancha.$ranchb.$ranchc.$ranchd.$ranche.$ranchf.$ranchg.$ranchh.$ranchi.$ranchj.$ranchk.$ranchl;
-
-setcookie("person", $username, time()+43200, "/admin/");
-setcookie("keys", $makekey, time()+43200, "/admin/");
-
+setcookie("person", $username, time()+43200);
+setcookie("keys", $makekey, time()+43200);
 //UPDATING USER KEY
 //mysql_query("UPDATE Users SET Cokey ='$makekey' WHERE Username ='$username'");
-
 $uquery = "UPDATE Users SET Cokey ='$makekey' WHERE Username = ?";
 $uid = $conn->prepare($uquery);
 $uid->bind_param('s', $username);
 $uid->execute();
 $uid->close();
-
 $Allowlogin = 11;
-
-
 }
 else {
+    //Brutecheck
+    $cbquery = "SELECT COUNT(*) AS usernamecount FROM BRUTE WHERE User = ?";
+    $cbch = $conn->prepare($cbquery);
+    $cbch->bind_param('s', $username);
+    $cbch->bind_result($usernamecount);
+    $cbch->execute();
+    $cbch->close();
+    if($usernamecount == 1){
+        $bquery = "INSERT INTO BRUTE (User, block_time) VALUES (?, NOW())";
+        $bch = $conn->prepare($bquery);
+        $bch->bind_param('s', $username);
+        $bch->execute();
+        $bch->close();
+    }else {
+            //als laatste foute login later is dan die daarvoor word de tries counter ge-reset.
+            $buquery = "UPDATE Brute SET tries=tries+1, block_time=NOW()  WHERE User = ? AND DATE_ADD(block_time, INTERVAL 30 MINUTE) >= NOW()";
+            $buch = $conn->prepare($buquery);
+            $buch->bind_param('s', $username);
+            $buch->execute();
+            $buch->close();
+            $buuuery = "UPDATE Brute SET tries = 1, block_time=NOW() WHERE User = ? AND DATE_ADD(block_time, INTERVAL 30 MINUTE) < NOW()";
+            $buu = $conn->prepare($buuuery);
+            $buu->bind_param('s', $username);
+            $buu->execute();
+            $buu->close();
+        }
+
+
 $Allowlogin = 10;
 }
-
 }
 else {
 $Allowlogin = 9;
 }
-
+}
+else {
+$Allowlogin = 2;
+}
 /*
 }
 */
 }
-
 }
 else {
 $Allowlogin = 8;
 }
-
 }
 else {
 $Allowlogin = 7;
 }
-
 }
 else {
 $Allowlogin = 6;
 }
-
 }
 else {
 $Allowlogin = 5;
 }
-
 }
 else {
 $Allowlogin = 4;
 }
-
 if ($Allowlogin ==1){
 echo '<!DOCTYPE HTML>
 <HTML>
@@ -317,7 +322,4 @@ echo "<P class='error'>Foutmelding<BR><A HREF='index.php'>terug</A></P>";
 echo '</BODY>
 </HTML>';
 }
-
-
 ?>
-
