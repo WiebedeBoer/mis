3f791b26d5a8366d108d71d7b9935be1a92bf967
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-01-19 12:25:37 +0100

    favicon
    favicon fix

--- includes/inc_head.php
+++ includes/inc_head.php
@@ -28,7 +28,7 @@ echo '<title>'.$describe_title.'</title>
 <META NAME="copyright" CONTENT="2015, Wiebe de Boer">
 <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
 <meta http-equive="content-type" content="text/html; charset=ISO-8859-1">
-<link rel="shortcut icon" type="image/png" href="favicon.png">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 <link rel="stylesheet" type="text/css" href="styles/'.$stylesheet.'.css">
 <meta name=viewport content="width=device-width, initial-scale=1">
 <script type="text/javascript" src="scripts/jquery.js"></script>
