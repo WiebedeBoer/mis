8d9358f98bedd938d141e53426ab2dd80c002085
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-01-23 11:38:49 +0100

    width and height banner
    width and height banner

--- admin/banner.php
+++ admin/banner.php
@@ -20,24 +20,48 @@ if ($user_cat =="superadmin" || $user_cat =="admin"){
 
 
 /*ONLY UPDATE*/
-if (isset($_POST["bannier"])){
+if (isset($_POST["bannier"]) && isset($_POST["width"]) && isset($_POST["height"])){
 $newtext = $_POST["bannier"];
+$newwidth = $_POST["width"];
+$newheight = $_POST["height"];
+
+//var_dump($newwidth);
 
 $enttext = str_replace("&"," ",$newtext);
 $apotext = str_replace("'"," ",$enttext);
 $mod_text = str_replace('"',' ',$apotext);
 
 if (filter_var($mod_text, FILTER_SANITIZE_STRING)){
+if (filter_var($newwidth, FILTER_VALIDATE_INT)){
+if (filter_var($newheight, FILTER_VALIDATE_INT)){
 /*
 mysql_query("UPDATE Seo SET Bannier ='$mod_text' WHERE ID ='1'");
 */
-$upquery = "UPDATE Seo SET Bannier =? WHERE ID ='1'";
+$upquery = "UPDATE Seo SET Bannier =?, Width =?, Height =? WHERE ID ='1'";
 $upid = $conn->prepare($upquery);
-$upid->bind_param('s', $modtext);
+$upid->bind_param('sii', $mod_text, $newwidth, $newheight);
 $upid->execute();
 $upid->close();
+/*
+$paginanum = 1;
+$upquery = "UPDATE Seo SET Bannier =? WHERE ID = ?";
+$upid = $conn->prepare($upquery);
+$upid->bind_param('si', $ugtext, $paginanum);
+$upid->execute();
+$upid->close();
+*/
 
-echo "<P>SEO geupdate</P>";
+echo "<P>banner geupdate</P>";
+
+}
+else {
+echo "<P class='error'>invalid integer</P>";
+}
+
+}
+else {
+echo "<P class='error'>invalid integer</P>";
+}
 
 }
 else {
@@ -52,16 +76,18 @@ $rowpho = mysql_fetch_array($resultpho);
 $bannier_text = $rowpho['Bannier'];
 */
 
-        $bquery = "SELECT Bannier FROM Seo WHERE ID ='1'";
+        $bquery = "SELECT Bannier, Width, Height FROM Seo WHERE ID ='1'";
         $bid = $conn->prepare($bquery);
         $bid->execute();
-        $bid->bind_result($bannier_text);
+        $bid->bind_result($bannier_text, $bannier_width, $bannier_height);
         $bid->fetch();
         $bid->close();
 
 
 echo '<FORM method="post" action="banner.php" id="myform" name="myform">';
-echo '<BR><input type="text" name="bannier" value="'.$bannier_text.'">
+echo '<BR>Banner: <input type="text" name="bannier" value="'.$bannier_text.'">
+<BR>Width: <input type="text" name="width" value="'.$bannier_width.'">
+<BR>Height: <input type="text" name="height" value="'.$bannier_height.'">
 <BR><input type="submit" value="update" class="knop">
 </FORM>';
 
